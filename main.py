5351a429de23c4f4a692640a1b7bc324399d32bc
DitDotz <zhiquan96@gmail.com>
2023-09-21 12:17:02 +0100

    Add more class attributes to Deck and Card class
    Add deck_name, correct_attempts, due_date, last_review_date, review_attempts, card_id and  edited incorrect_attempts to current_incorrect_attempts for Card class. Add deck_id to Deck class.

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 import random
+import datetime
 
+class Card:
+    card_counter = 1  # Class-level counter for generating unique card IDs
 
-class Flashcard:
-    def __init__(
-        self, question, answer, interval=1, ease_factor=2.5, incorrect_attempts=0
-    ):
+    def __init__(self, question, answer, interval=1, ease_factor=2.5):
+        self.card_id = Card.card_counter  # Unique identifier for the card
+
         self.question = question
         self.answer = answer
+        self.deck_name = None  # Initialize deck_name as None
         self.interval = interval
         self.ease_factor = ease_factor
-        self.incorrect_attempts = incorrect_attempts
+        self.correct_attempts = 0
+        self.current_incorrect_attempts = 0
+        self.due_date = datetime.date.today()  # Initial due date (today)
+        self.last_review_date = None  # Last review date
+        self.review_attempts = 0  # Total review attempts
+        Card.card_counter +=1
 
     def edit_card(self, new_question, new_answer):
         self.question = new_question
         self.answer = new_answer
 
-
 class Deck:
-    def __init__(self, name):
+    deck_counter = 1 # Class-level counter for generating unique deck IDs
+
+    def __init__(self, name=None):
         self.name = name
         self.cards = []
+        self.deck_id = Deck.deck_counter
+        Deck.deck_counter += 1  # Increment the deck ID counter
 
     def add_card(self, card):
+        card.deck_name = self.name  # Set the deck_name attribute in the Card class
         self.cards.append(card)
 
     def remove_card(self, card):
@@ -41,32 +53,40 @@ class Deck:
             card = self.cards[0]
 
             print(f"Question: {card.question}")
-            user_response = input("Your answer: ").strip()
+            user_response = input("Your answer: ").strip().lower()
+            card.review_attempts += 1  # Increment review attempts
+            card.last_review_date = datetime.date.today()  # Update last review date
 
             if user_response == card.answer.strip().lower():
-                if card.incorrect_attempts >= 1:
-                    card.ease_factor -= 0.1
+
+                if card.current_incorrect_attempts >= 1:
+                    card.correct_attempts += 1
                     card.interval *= (
                         card.ease_factor
                     )  # Decrease the interval for incorrect answers
                     self.cards.pop(0)  # Remove the card from the review queue
-                    card.incorrect_attempts = (
+                    card.current_incorrect_attempts = (
                         0  # Reset incorrect attempts for next review
                     )
+                    card.due_date += datetime.timedelta(days=int(card.interval))
                     print("Correct!")
-                    print(f"New interval: {card.interval:.1f} days\n")
+                    print(', '.join("%s: %s" % item for item in vars(card).items()))
 
                 else:
+                    card.correct_attempts += 1
                     card.interval *= (
                         card.ease_factor
                     )  # Increase the interval for correct answers
                     self.cards.pop(0)  # Remove the card from the review queue
+                    card.due_date += datetime.timedelta(days=int(card.interval))
+            
                     print("Correct!")
-                    print(f"New interval: {card.interval:.1f} days\n")
+                    print(', '.join("%s: %s" % item for item in vars(card).items()))
 
             else:
                 card.interval = 1  # Reset the interval for incorrect answers
-                card.incorrect_attempts += 1
+                card.ease_factor -= 0.1
+                card.current_incorrect_attempts += 1
                 self.cards.append(
                     self.cards.pop(0)
                 )  # Move the card to the end of the queue
@@ -80,15 +100,15 @@ class Deck:
 
 
 # Create cards
-card1 = Flashcard("What is the capital of France?", "Paris")
-card2 = Flashcard("What is your name?", "ZQ")
+card1 = Card("What is the capital of France?", "Paris")
+card2 = Card("What is your name?", "ZQ")
 
-# # Create a deck
-# my_deck = Deck("Test deck")
+# Create a deck
+my_deck = Deck("Test deck")
 
-# # Add cards to deck
-# my_deck.add_card(card1)
-# my_deck.add_card(card2)
+# Add cards to deck
+my_deck.add_card(card1)
+my_deck.add_card(card2)
 
-# # Review deck
-# my_deck.review()
+# Review deck
+my_deck.review()
